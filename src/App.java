f46d0b129d5bba5372074573375812f70b8fbe11
Angel19823 <acardenasl2@est.ups.edu.ec>
2024-09-27 12:44:59 -0500

    Seleccion

--- src/App.java
+++ src/App.java
@@ -1,10 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Metodo de ordenamiento Seleccion");
-        int []arreglo = {10, 5, 9, 3, 1};
+        int []arreglo = {10, 5, 8, 2, 0};
         
         MetodoOrdenamiento mO = new MetodoOrdenamiento();
-        mO.sortBySeleccion(arreglo);
+        mO.sortBySeleccion(arreglo, true);
+        mO.printArreglo(arreglo);
+
+        System.out.println();
+        mO.sortBySeleccion(arreglo, false);
         mO.printArreglo(arreglo);
     }
 }
--- src/MetodoOrdenamiento.java
+++ src/MetodoOrdenamiento.java
@@ -1,15 +1,29 @@
 public class MetodoOrdenamiento {
 
-    public int[] sortBySeleccion(int[] arreglo){
+    public int[] sortBySeleccion(int[] arreglo, boolean asendente){
         int tamano = arreglo.length;
 
         //Recorre el arreglo
         for (int i = 0; i < tamano; i++) {
             int indice = i;
             for (int j = i+1; j < tamano; j++) {
-                if(arreglo[j] < arreglo[indice]){
-                    indice= j;
+                if (asendente) {
+                    if(arreglo[j] < arreglo[indice]){
+                        indice= j;
+                    }
+                }else{
+                    if(arreglo[j] > arreglo[indice]){
+                        indice= j;
+                    }
                 }
+                //PRGO TIP
+                
+                //if(asendente
+                //? arreglo[j] < arreglo[indice]
+                //: arreglo[j] > arreglo[indice]){
+                //    indice=j;
+                //}
+                
             }
             //Intercambio de indice encontrado por el primero
             int aux = arreglo[indice];
